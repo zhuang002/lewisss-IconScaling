1c0c28152a4c66ada4f1486a3e4ef1e7e1bbe41e
zhuang002 <zhuang001@gmail.com>
2026-07-23 15:31:43 -0400

    V2.0 using top down methodology

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,35 @@ public class Main {
 				{'*', ' ', '*'}  // the third element is an array.
 		}; // array of arrays == 2D array. each element of the array is an array.
 		
-		String[] s = {"", "", ""};
-		
-		for (int i=0;i<lines.length;i++) { // build each line, the ith line, totally lines.length lines
-			for (int j=0;j<lines[0].length;j++) { // duplicate the jth character of ith line, totally line[i].length characters
-				for (int k=0;k<scale;k++) {
-					s[i] += lines[i][j]; // concat the ith line with the jth character of the original  ith line.
-				}
-			}
+		for (int i=0;i<lines.length;i++) {
+			String s = buildString(lines[i],scale);
+			printScaledLine(s,scale);
 		}
-		
-		for (int i=0;i<lines.length;i++) {// print ith line, totally scale lines.
-			for (int j=0;j<scale;j++) { // print each line scale times. 
-				System.out.println(s[i]); // print the ith line.
-			}
-		}
-		
+	}
 
+	private static void printScaledLine(String s, int scale) {
+		for (int i=0;i<scale;i++) {
+			System.out.println(s);
+		}
+		
+	}
+
+	private static String buildString(char[] line, int scale) {
+		String s = "";
+		for (int i=0;i<line.length;i++) {
+			s += scaleCharacter(line[i],scale);
+		}
+		return s;
+	}
+
+	private static String scaleCharacter(char c, int scale) {
+		// TODO Auto-generated method stub
+		String s = "";
+		
+		for (int i=0;i<scale;i++) {
+			s+=c;
+		}
+		return s;
 	}
 
 }
